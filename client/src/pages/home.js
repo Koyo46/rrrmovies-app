a35ed699a186203d1fe1cc3df17a6fb259e6c989
reiken <ultee529@outlook.jp>
2023-12-01 00:30:07 +0900

    dashboard->home rename, get popular movies

---
+++ client/src/pages/home.js
@@ -0,0 +1,36 @@
+import AppLayout from '@/components/Layouts/AppLayout'
+import axios from 'axios'
+import Head from 'next/head'
+import { useEffect, useState } from 'react'
+
+const Dashboard = () => {
+    const[movies,setMovies] = useState([])
+    useEffect(() => {
+        const fetchMovies = async() => {
+            try {
+                const response = await axios.get('api/getPopularMovies');
+                // console.log(response.data.results);
+                setMovies(response.data.results);
+                console.log(movies);
+            } catch (error) {
+                console.log(err);
+            }
+        }
+        fetchMovies();
+    },[])
+    return (
+        <AppLayout
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Home
+                </h2>
+            }>
+            <Head>
+                <title>Laravel - Dashboard</title>
+            </Head>
+
+        </AppLayout>
+    )
+}
+
+export default Dashboard
