5262b0a1d069b55fcf5ef15e27b6a75897f1afee
reiken <ultee529@outlook.jp>
2023-12-02 22:43:25 +0900

    MUIを使って詳細画面の背景画像を追加

--- client/src/pages/detail/[media_type]/[media_id]/index.jsx
+++ client/src/pages/detail/[media_type]/[media_id]/index.jsx
@@ -1,9 +1,72 @@
+import { Box, Container, Grid } from '@mui/material'
+import axios from 'axios'
 import React from 'react'
 
-const Detail = () => {
-  return (
-    <div>作品詳細ページ</div>
-  )
+const Detail = ({ detailData }) => {
+    console.log(detailData)
+
+    return (
+        <Box
+            sx={{
+                height: '70vh',
+                bgcolor: 'red',
+                position: 'relative',
+            }}>
+            <Box
+                sx={{
+                    backgroundImage: `url(https://image.tmdb.org/t/p/original/${detailData.backdrop_path})`,
+                    position: 'absolute',
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
+
+                    '&::before': {
+                        content: '""',
+                        position: 'absolute',
+                        top: 0,
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        background: 'rgba(0,0,0,0.5)',
+                        backdropFilter: 'blur(10px)',
+                    },
+                }}>
+                <Container>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12} md={6}></Grid>
+                        <Grid item xs={12} md={6}></Grid>
+                    </Grid>
+                </Container>
+            </Box>
+        </Box>
+    )
 }
 
+// SSR
+export async function getServerSideProps(context) {
+    const { media_type, media_id } = context.params
+
+    try {
+        console.log(process.env.TMDB_API_KEY)
+        const res = await axios.get(
+            `https://api.themoviedb.org/3/${media_type}/${media_id}?api_key=${process.env.TMDB_API_KEY}&language=ja-JP`,
+        )
+        process.env.TMDB_API_KEY
+        const fetchData = res.data
+
+        return {
+            props: {
+                detailData: fetchData,
+            },
+        }
+    } catch {
+        return {
+            notFound: true,
+        }
+    }
+}
 export default Detail
