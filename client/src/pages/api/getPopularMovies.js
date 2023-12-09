e1bbc413784e8826071692f07702e924d94ddc66
reiken <ultee529@outlook.jp>
2023-12-09 18:24:28 +0900

    フロントにエラー内容の詳細を送らないようにする

--- client/src/pages/api/getPopularMovies.js
+++ client/src/pages/api/getPopularMovies.js
@@ -1,12 +1,14 @@
-import axios from "axios";
+import axios from 'axios'
 
-export default async function handler(req,res){
+export default async function handler(req, res) {
     try {
-        const response = await axios(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=ja-JP`)
-        res.status(200).json(response.data);
-        console.log('取得した結果は',response.data)
+        const response = await axios(
+            `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=ja-JP`,
+        )
+        res.status(200).json(response.data)
+        console.log('取得した結果は', response.data)
     } catch (error) {
-        console.log(err);
-        res.status(500).json('エラーが発生しました',err);
+        console.log(err)
+        res.status(500).json({ message: 'エラーが発生しました' })
     }
 }
