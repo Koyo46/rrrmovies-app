850d727754adcb940831f50323ba807c50dd1432
reiken <ultee529@outlook.jp>
2023-12-09 19:31:59 +0900

    検索結果を検索画面のコンソールに表示できるよう実装

---
+++ client/src/pages/api/searchMedia.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+export default async (req, res) => {
+    const { searchQuery } = req.query
+    console.log(searchQuery)
+
+    if (!searchQuery) {
+        res.status(400).json({ message: '検索文字がありません' })
+        console.log('検索文字がありません')
+    }
+
+    try {
+        const response = await axios(
+            `https://api.themoviedb.org/3/search/multi?api_key=${
+                process.env.TMDB_API_KEY
+            }&language=ja-JP&query=${encodeURIComponent(searchQuery)}`,
+        )
+        return res.status(200).json(response.data)
+    } catch (err) {
+        console.log('エラー内容は', err)
+        return res.status(500).json({ message: 'エラーが発生しました' })
+    }
+}
--- client/src/pages/search.jsx
+++ client/src/pages/search.jsx
@@ -1,6 +1,39 @@
+import axios from 'axios'
+import { useRouter } from 'next/router'
 import React from 'react'
+import { useEffect } from 'react'
 
 const search = () => {
+    const [searchResults, setSearchResults] = React.useState([])
+    const router = useRouter()
+    const { query: searchQuery } = router.query
+    console.log(searchQuery)
+
+    useEffect(() => {
+        if (!searchQuery) return
+        const fetchMedia = async () => {
+            try {
+                const response = await axios.get(
+                    `api/searchMedia?searchQuery=${searchQuery}`,
+                )
+                console.log(response.data)
+                const searchResults = response.data.results
+                console.log(searchResults)
+
+                const validResults = searchResults.filter(
+                    item =>
+                        item.media_type === 'movie' || item.media_type === 'tv',
+                )
+                console.log(validResults)
+                setSearchResults(validResults)
+                console.log(searchResults)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        fetchMedia()
+    }, [searchQuery])
     return <div>search</div>
 }
 
